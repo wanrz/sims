582fc771a1b7360f4a15e4fd453545bc05cff7e6
wanrenzong <wangrenzong@cloudwalk.cn>
2019-01-12 11:18:04 +0800

    提交表结构

---
+++ doc/sql/sims.sql
@@ -0,0 +1,94 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.5.37 : Database - db_studentinfo
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_studentinfo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `db_studentinfo`;
+
+/*Table structure for table `f_people` */
+
+DROP TABLE IF EXISTS `f_people`;
+
+CREATE TABLE `f_people` (
+  `id` varchar(32) NOT NULL,
+  `name` varchar(256) DEFAULT NULL,
+  `title` varchar(256) DEFAULT NULL,
+  `state` varchar(1) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  `remark` varchar(512) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `f_picture` */
+
+DROP TABLE IF EXISTS `f_picture`;
+
+CREATE TABLE `f_picture` (
+  `id` varchar(32) NOT NULL,
+  `name` varchar(256) DEFAULT NULL,
+  `style` varchar(32) DEFAULT NULL,
+  `size` bigint(20) DEFAULT NULL,
+  `path` varchar(256) DEFAULT NULL,
+  `people_id` varchar(32) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_grade` */
+
+DROP TABLE IF EXISTS `t_grade`;
+
+CREATE TABLE `t_grade` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gradeName` varchar(20) DEFAULT NULL,
+  `gradeDesc` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_student` */
+
+DROP TABLE IF EXISTS `t_student`;
+
+CREATE TABLE `t_student` (
+  `stuId` int(11) NOT NULL AUTO_INCREMENT,
+  `stuNo` varchar(20) DEFAULT NULL,
+  `stuName` varchar(10) DEFAULT NULL,
+  `sex` varchar(5) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `gradeId` int(11) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `stuDesc` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`stuId`),
+  KEY `FK_t_student` (`gradeId`),
+  CONSTRAINT `FK_t_student` FOREIGN KEY (`gradeId`) REFERENCES `t_grade` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `picture` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
