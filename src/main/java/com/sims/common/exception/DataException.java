4c39a08bfc84616d4eff2bbdaa1a1e6020409748
wanrz <32393094+wanrz@users.noreply.github.com>
2019-01-15 16:11:05 +0800

    Update DataException.java

--- src/main/java/com/sims/common/exception/DataException.java
+++ src/main/java/com/sims/common/exception/DataException.java
@@ -4,7 +4,6 @@ package com.sims.common.exception;
  * ClassName:DataException <br/>
  * Description: 数据异常类类 <br/>
  * Date:     2015年5月16日 上午11:41:41 <br/>
- * @author   Jackson He
  * @version
  * @see
  */
